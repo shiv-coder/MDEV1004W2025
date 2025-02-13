7be873b706a6f34e573b81088ce7dae634a699d8
shiv-coder <shivi359@gmail.com>
2025-02-13 10:21:46 -0500

    Completed login function

--- Week6Express/src/controllers/authController.js
+++ Week6Express/src/controllers/authController.js
@@ -32,3 +32,35 @@ exports.registerUser = async(req,res)=>{
         return res.status(500).json({message:'Error registering user'});
     }
 }
+
+exports.loginUser = async(req,res)=>{
+    const{email,password}=req.body;
+
+    try{
+            //Validate
+            if(!email || !password){
+                return res.status(401).json({message:'All fields are required'});
+            }
+
+           
+            //Check for existing user
+
+            const user = await User.findOne({email});
+            if(!user){
+                return res.status(401).json({message:'Imvalid email or password'});
+            }
+            //Compare password with hashed password
+            const isPasswordValid = await bcrypt.compare(password,user.password);
+            if(!isPasswordValid){
+                return res.status(401).json({message:'Password does not match'});
+            }
+            //Succseful login
+            res.status(200).json({message:'Login succesful',userId:user._id});       
+          
+    }
+
+    catch(error){
+        console.error('Error details: ',error);
+        res.status(500).json({message:'Error loggin in user'});
+    }
+}
