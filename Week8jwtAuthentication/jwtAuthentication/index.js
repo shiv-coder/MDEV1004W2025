8ef6d5d24a60285642149acd959d4f94310494fe
shiv-coder <shivi359@gmail.com>
2025-03-06 10:17:14 -0500

    complete middleware

--- Week8jwtAuthentication/jwtAuthentication/index.js
+++ Week8jwtAuthentication/jwtAuthentication/index.js
@@ -1,7 +1,13 @@
 const http = require('http');
 const routes = require('./routes/routes');
 const server = http.createServer(routes);
+const express = require('express');
+const app = express();
+const verifyToken = require('./middleware/auth');
 require('dotenv').config();
+
+
+//Protected route-Lab 5
 
 const { API_PORT} = process.env;
 const port = process.env.PORT || API_PORT;
