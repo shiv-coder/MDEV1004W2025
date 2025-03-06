4886654640fcc033f60efabcc205709259d18725
shiv-coder <shivi359@gmail.com>
2025-03-06 11:28:30 -0500

    completed routes file

--- Week8jwtAuthentication/jwtAuthentication/routes/routes.js
+++ Week8jwtAuthentication/jwtAuthentication/routes/routes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const jwtController = require('../controllers/controllers');
+const jwtmiddleware = require('../middleware/auth');
+
+router.post('/register',jwtController.register);
+router.post('/login',jwtController.login);
+
+//create/welcome endpoint ---call middleware & welcome function-Lab5
+
+module.exports = router;
+
 
